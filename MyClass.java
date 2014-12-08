d39e3e6fd5553ea8ce847f9b94ffd6c08795a909
ChenHuachong <huachongchen@sohu-inc.com>
2014-12-08 15:28:17 +0800

    又一次修改MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,6 @@
 public class MyClass{
   public static void main(String args[]){
+    public static final String Tag="MyClass";
     System.out.println("Hello World!");
     String str="Hello";
     String strLink="http://www.baidu.com";
